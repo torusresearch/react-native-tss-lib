f141f84884ef1da11e0e6e2bf5b4cbaa102f958b
ieow <cwlee0@gmail.com>
2024-06-14 09:56:48 +0800

    feat: match mpc v3 interface

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,10 @@
-export * from './tsslib';
+import * as lib from './tsslib';
 export * from './Bridge';
+
+const tssLib = {
+    keyType: "secp256k1",
+    lib : lib,
+};
+  
+export { tssLib };
+export default lib;
